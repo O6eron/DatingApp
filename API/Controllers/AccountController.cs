812f54fcae8ac99f689aa6afaa42e340e243f0a5
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2021-11-24 15:44:42 -0600

    Created register dto. Updated AccountController to use dto and existing user check.

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+  public class AccountController : BaseApiController
+  {
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if(await UserExists(registerDto.UserName))
+        {
+            return BadRequest("UserName is taken");
+        }
+        else
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+
+    private async Task<bool> UserExists(string userName)
+    {
+        return await _context.Users.AnyAsync( user => user.UserName == userName.ToLower());
+    }
+  }
+}
