0a14bc9e554f48a0c087e50fda15f8b100763aea
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2021-11-30 12:00:09 -0600

    Created prevent-unsaved-changes guard for the member edit component. Created prevent tab closing confirmation for member edit component.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { NotFoundComponent } from './_errors/not-found/not-found.component';
 import { ServerErrorComponent } from './_errors/server-error/server-error.component';
 import { TestErrorsComponent } from './_errors/test-errors/test-errors.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -20,7 +21,7 @@ const routes: Routes = [
     children: [
       {path: 'members', component: MemberListComponent},
       {path: 'members/:username', component: MemberDetailComponent},
-      {path: 'member/edit', component: MemberEditComponent},
+      {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
     ]
