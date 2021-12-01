c39e9d611b3426a312283cac8408fea68ee9d519
Anton Grygoriev <agrygoriev@Academic.RRC.ca>
2021-11-30 18:07:58 -0600

    Updated members.service.ts and photo editor component by adding photo deletion functionality

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -44,6 +44,12 @@ export class PhotoEditorComponent implements OnInit {
     })
   }
 
+  deletePhoto(photoId: number) {
+    this.membersService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(p => p.id !== photoId);
+    })
+  }
+
   initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/add-photo',
